bcb133d583ba9538bab9bc3f1e7350b7484d6422
StevenSmith-code <86922292+StevenSmith-code@users.noreply.github.com>
2021-12-19 14:01:20 -0600

    added post component and ability to render posts from firebase

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,24 @@
 import { SparklesIcon } from "@heroicons/react/outline";
+import { useEffect, useState } from "react";
 import Input from "./Input";
-
+import { onSnapshot, collection, query, orderBy } from "@firebase/firestore";
+import { db } from "../firebase";
+import Post from "./Post";
+import { useSession } from "next-auth/react";
 function Feed() {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
   return (
     <div className='flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]'>
       <div className='text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700'>
@@ -13,9 +30,9 @@ function Feed() {
 
       <Input />
       <div className='pb-72'>
-        {/* {posts.map((post) => (
-        <Post key={post.id} id={post.id} post={post.data()} />
-      ))} */}
+        {posts.map((post) => (
+          <Post key={post.id} id={post.id} post={post.data()} />
+        ))}
       </div>
     </div>
   );
---
+++ components/Post.jsx
@@ -0,0 +1,64 @@
+import { DotsHorizontalIcon } from "@heroicons/react/outline";
+
+function Post({ id, post, postPage }) {
+  return (
+    <div className='p-3 flex cursor-pointer border-b border-gray-700'>
+      {!postPage && (
+        <img
+          src={post?.userImg}
+          alt='user image'
+          className='h-11 w-11 rounded-full mr-4'
+        />
+      )}
+      <div className='flex flex-col space-y-2 w-full'>
+        <div className={`flex ${!postPage && "justify-between"}`}>
+          {postPage && (
+            <img
+              src={post?.userImg}
+              alt='user image'
+              className='h-11 w-11 rounded-full mr-4'
+            />
+          )}
+          <div className='text-[#6e767d]'>
+            <div className='inline-block group'>
+              <h4
+                className={`font-bold text-[15px] sm:text-base text-[#d9d9d9] group-hover:underline ${
+                  !postPage && "inline-block"
+                }`}>
+                {post?.username}
+              </h4>
+              <span
+                className={`text-sm sm:text-[15px] ${!postPage && "ml-1.5"}`}>
+                @{post?.tag}
+              </span>
+            </div>{" "}
+            .{" "}
+            <span className='hover:underline text-sm sm:text-[15px] '>
+              {/* <Moment fromNow>{post?.timestamp?.toDate()}</Moment> */}
+            </span>
+            {!postPage && (
+              <p className='text-[#d9d9d9] text-[15px] sm:text-base mt-0.5'>
+                {post?.text}
+              </p>
+            )}
+          </div>
+          <div className='icon group flex-shrink-0 ml-auto'>
+            <DotsHorizontalIcon className='h-5 text-[#6e767d] group-hover:text-[#1d9bf0]' />
+          </div>
+        </div>
+        {postPage && (
+          <p className='text-[#d9d9d9] text-[15px] sm:text-base mt-0.5'>
+            {post?.text}
+          </p>
+        )}
+        <img
+          src={post?.image}
+          alt=''
+          className='rounded-2xl max-h-[700px] object-cover mr-2'
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Post;
