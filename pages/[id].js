2e4b95c72ceb5cb7723b104aad11afdad63d0401
StevenSmith-code <86922292+StevenSmith-code@users.noreply.github.com>
2021-12-22 17:46:50 -0600

    Added comment component

--- pages/[id].js
+++ pages/[id].js
@@ -17,14 +17,15 @@ import { db } from "../firebase";
 import { ArrowLeftIcon } from "@heroicons/react/solid";
 import Head from "next/head";
 import Login from "../components/Login";
+import Comment from "../components/Comment";
 
 function PostPage({ trendingResults, followResults, providers }) {
   const { data: session } = useSession();
   const [isOpen, setIsOpen] = useRecoilState(modalState);
   const router = useRouter();
   const { id } = router.query;
+  const [comments, setComments] = useState([]);
   const [post, setPost] = useState();
-
   useEffect(
     () =>
       onSnapshot(doc(db, "posts", id), (snapshot) => {
@@ -32,19 +33,54 @@ function PostPage({ trendingResults, followResults, providers }) {
       }),
     [db]
   );
-  if (!session) {
-    return <Login providers={providers} />;
-  }
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "posts", id, "comments"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot) => setComments(snapshot.docs)
+      ),
+    [db, id]
+  );
+
+  if (!session) return <Login providers={providers} />;
+
   return (
-    <div className=''>
+    <div>
       <Head>
         <title>
           {post?.username} on Twitter: "{post?.text}"
         </title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className='bg-black min-h-screen flex max-w-[1500px] mx-auto '>
+      <main className='bg-black min-h-screen flex max-w-[1500px] mx-auto'>
         <Sidebar />
-        {/* Widgets */}
+        <div className='flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]'>
+          <div className='flex items-center px-1.5 py-2 border-b border-gray-700 text-[#d9d9d9] font-semibold text-xl gap-x-4 sticky top-0 z-50 bg-black'>
+            <div
+              className='hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0'
+              onClick={() => router.push("/")}>
+              <ArrowLeftIcon className='h-5 text-white' />
+            </div>
+            Tweet
+          </div>
+
+          <Post id={id} post={post} postPage />
+          {comments.length > 0 && (
+            <div className='pb-72'>
+              {comments.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  id={comment.id}
+                  comment={comment.data()}
+                />
+              ))}
+            </div>
+          )}
+        </div>
 
         {isOpen && <Modal />}
       </main>
